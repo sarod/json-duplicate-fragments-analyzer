8e90f9b0c4a11b8864d844a7da52f03d54d83fa3
Sebastien Arod <sbnarod@gmail.com>
2019-06-04 14:59:43 +0200

    Updates

---
+++ precache-manifest.b6920dd54aa4c2080629e1055f965bb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07f1ea9e47353080df2612ad55feab2",
+    "url": "/json-duplicate-fragments-analyzer/index.html"
+  },
+  {
+    "revision": "7819797ffdbfacd2c5be",
+    "url": "/json-duplicate-fragments-analyzer/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "8b91d2d669c29ad3a610",
+    "url": "/json-duplicate-fragments-analyzer/static/js/2.803b15be.chunk.js"
+  },
+  {
+    "revision": "7819797ffdbfacd2c5be",
+    "url": "/json-duplicate-fragments-analyzer/static/js/main.c042e0b5.chunk.js"
+  },
+  {
+    "revision": "e58af7ebb453dd5605aa",
+    "url": "/json-duplicate-fragments-analyzer/static/js/runtime~main.4caf13c7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/json-duplicate-fragments-analyzer.git/precache-manifest.a452af1dac40f88069cff4e0de237b59.js"
+  "/json-duplicate-fragments-analyzer/precache-manifest.b6920dd54aa4c2080629e1055f965bb8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/json-duplicate-fragments-analyzer.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/json-duplicate-fragments-analyzer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
