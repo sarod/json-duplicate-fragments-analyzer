d703cb1c65bb6553011bc50dd333ebe1f657a689
Sebastien Arod <sbnarod@gmail.com>
2019-06-04 16:18:06 +0200

    Updates

--- precache-manifest.e55fd10fcd279474ab79fd47c13978a3.js
+++ precache-manifest.e55fd10fcd279474ab79fd47c13978a3.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d560bd8f3251235d28c02d9d25e476ad",
+    "revision": "e2ca970d00176b51f443090eb2f67423",
     "url": "/json-duplicate-fragments-analyzer/index.html"
   },
   {
-    "revision": "ea37de4c5ece270add71",
+    "revision": "2a6a21d82b6564d671cb",
     "url": "/json-duplicate-fragments-analyzer/static/css/main.27fd15f6.chunk.css"
   },
   {
-    "revision": "ae04990653b169bcdbe9",
-    "url": "/json-duplicate-fragments-analyzer/static/js/2.0e32f90c.chunk.js"
+    "revision": "30fedd11721b3a58da12",
+    "url": "/json-duplicate-fragments-analyzer/static/js/2.8a5960e0.chunk.js"
   },
   {
-    "revision": "ea37de4c5ece270add71",
-    "url": "/json-duplicate-fragments-analyzer/static/js/main.9c268b6e.chunk.js"
+    "revision": "2a6a21d82b6564d671cb",
+    "url": "/json-duplicate-fragments-analyzer/static/js/main.2adee5b6.chunk.js"
   },
   {
     "revision": "e58af7ebb453dd5605aa",
